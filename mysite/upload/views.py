d6b9e0cb368d864b60434fa7488c11ba613e7570
Nirav <nivi@nirav.(none)>
2014-02-07 00:05:02 +0530

    Code optimization

--- mysite/upload/views.py
+++ mysite/upload/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.views.decorators.csrf import csrf_exempt
 
 from upload.forms import UserFileForm
 from upload.models import Ufile, UserProfile
@@ -9,55 +8,61 @@ def demo(request):
     params = {}
     success = False
     file_name = None
-    form = UserFileForm()
     try:
         if request.method == 'POST':
-            pf_user = request.POST.get('user_email')
-
-            try:
-                profile_user = UserProfile.objects.get(user_email=pf_user)
-            except Exception, e:
-                print "User Error ==> %s" %str(e)
-                profile_user=None
-
-            if profile_user:
-                form = UserFileForm(request.POST, request.FILES)
-                print "POST method"
-                if form.is_valid():
-                    print "form is valid"
-                    #form.save()
+            #
+            # Create instance of UserFileForm object
+            #
+            form = UserFileForm(request.POST, request.FILES)
+            if form.is_valid():
+                print "form is valid"
+                #
+                # Get the user's email ID and check whether user exists or not. 
+                #
+                pf_user_email = request.POST.get('user_email')
+                try:
+                    profile_user = UserProfile.objects.get(user_email=pf_user_email)
+                except:
+                    profile_user = None
+                #
+                # Store file name and if user exists then add uploaded file to that user's account. 
+                #
+                file_name = request.FILES['user_file']
+                if profile_user:
+                    print "Old User"
                     ufile = Ufile.objects.create(user_file=file_name, user_profile = profile_user)
                     ufile.save()
-                    print "form saved"
+                    print "File saved"
                     success = True
-                    file_name = request.FILES['user_file']
-
+                #
+                # If new user arrives.
+                #
                 else:
-                    form = UserFileForm(request.POST)
-                    print "form is not valid"
-                    print "Error ==> ", form.errors
+                    print "New User"
+                    uprofile = UserProfile.objects.create(user_email=pf_user_email)
+                    uprofile.save()
+                    print "Email saved"
+                    ufile = Ufile.objects.create(user_file=file_name,user_profile=uprofile)
+                    ufile.save()
+                    print "File saved"
+                    success = True
 
+            #
+            # Form is invalid.
+            #
             else:
-                form = UserFileForm(request.POST, request.FILES)
-                if form.is_valid():
-                    user_profile = UserProfile.objects.create(user_email=pf_user)
-                    user_profile.save()
-                    file_name = request.FILES['user_file']
-                    user_file = Ufile.objects.create(user_file=file_name,user_profile=user_profile)
-                    user_file.save()
-                    success = True
-                    file_name = request.FILES['user_file']
-                else:
-                    form = UserFileForm(request.POST)
-                    print "form is not valid"
-                    print "Error ==> ", form.errors
+                form = UserFileForm(request.POST)
+                print "form is not valid"
+                print "Form Error : ", form.errors
 
-
+        #
+        # Get method
+        #
         else:
             form = UserFileForm()
             
     except Exception, e:
-        print "Error is: %s" %str(e)
+        print "Error is: ", str(e)
 
     params['form'] = form
     params['success'] = success
