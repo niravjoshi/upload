91ca96566fc6ca7c443d3f48a0aaff7027941973
Nirav <nivi@nirav.(none)>
2014-02-22 01:25:39 +0530

    Validation for signup and login form.

--- mysite/upload/views.py
+++ mysite/upload/views.py
@@ -7,59 +7,95 @@ from django.contrib.auth.models import User
 from upload.forms import UserFileForm, UserForm
 from upload.models import Ufile, UserProfile
 
-def signup(request):
+def login_page(request):
     params = {}
     if request.method == "POST" :
         try:
-            form = UserForm(request.POST)
-            params['form'] = form
-            if form.is_valid():
-                email = form.cleaned_data['email']
-                try:
-                    user = User.objects.get(email=email)
-                except Exception, e:
-                    print "User Error: %s" %str(e)
-                    user = None
-
-                if user:
-                    user_exists = None
-                    params['user_exists'] = "User with %s email-id already exists." %email
-
+            #
+            # Checking if its login or signup request.
+            #
+            if request.POST.get('login'):
+                email = request.POST.get('email')
+                passwd = request.POST.get('password')
+                if email and passwd:
+                    #
+                    # login request
+                    #
+                    user = authenticate(email=email, password=passwd)
+                    print "user: ", user
+                    if user is not None:
+                        if user.is_active:
+                            login(request, user)
+                            logged = None
+                            params['logged'] = "Logged in successfully"
+                            return render_to_response('top.html', params, context_instance=RequestContext(request))
+                    else:
+                        invalid = None
+                        params['invalid'] = "Invalid email-id or password"
+                        return render_to_response('login.html', params, context_instance=RequestContext(request))
                 else:
-                    passwd = form.cleaned_data['password']
-                    fname = form.cleaned_data['first_name']
-                    lname = form.cleaned_data['last_name']
+                    required = None
+                    params['required'] = "Both fields are required"
+                    return render_to_response('login.html', params, context_instance=RequestContext(request))
+            #
+            # Signup request
+            #
+            else:
+                form = UserForm(request.POST)
+                if form.is_valid():
+                    email = form.cleaned_data['email']
+                    #
+                    # Check if user exists with given email-id.
+                    #
+                    try:
+                        user = User.objects.get(email=email)
 
-                    new_user = User.objects.create_user(fname, email, password=passwd, last_name=lname)
-                    new_user.save()
-                    print "user saved"
+                    except Exception, e:
+                        print "User Error: %s" %str(e)
+                        user = None
+                    #
+                    # Check if user's email-id is already used and if used give an error.
+                    #
+                    if user:
+                        user_exists = None
+                        params['user_exists'] = "User with %s email-id already exists." %email
+                    #
+                    # New user registration
+                    #
+                    else:
+                        passwd = form.cleaned_data['password']
+                        fname = form.cleaned_data['first_name']
+                        lname = form.cleaned_data['last_name']
+                        new_user = User.objects.create_user(fname, email, password=passwd, last_name=lname)
+                        new_user.save()
+                        print "user saved"
+                        print "fname: %s\nlname: %s\nemail: %s\npasswd: %s" %(fname,lname,email,passwd)
+                        params['form'] = form
+                        user_created = None
+                        params['user_created'] = "Congrats!!! Account created successfully."
+                #
+                # Form is invalid
+                #
+                else:
+                    if "@" and ".com" not in request.POST.get('email') and request.POST.get('first_name'):
+                        valid_email = None
+                        params['valid_email'] = "Please enter a valid email address."
+                    else:
+                        valid_input = None
+                        params['valid_input'] = "All fields are required."
 
-                    print "fname: %s\nlname: %s\nemail: %s\npasswd: %s" %(fname,lname,email,passwd)
-                    params['form'] = form
-                    user_created = None
-                    params['user_created'] = "Congrats!!! Account created successfully."
-                    """
-                    TO DO: Create new user
-                    """
+
+
         except Exception, e:
+            params['form'] = form
+            some_error = None
+            params ['some_error'] = "Oh snap! Something went wrong!!!\n Please try again."
             print "Login Error: %s" %str(e)
-    #user = authenticate(email=email, password=passwd)
-    #print "user: ", user
-    #if user is not None:
-    #    if user.is_active:
-    #        login(request, user)
-    #        return render_to_response('top.html', params, context_instance=RequestContext(request))
-    #else:
-    #    return render_to_response('login.html', params, context_instance=RequestContext(request))
     else:
         params['form'] = UserForm()
 
     return render_to_response('login.html', params, context_instance=RequestContext(request))
 
-def login(request):
-    params = {}
-    return render_to_response('singup.html', params, context_instance=RequestContext(request))
-   
 #@login_required
 def demo(request):
     params = {}
