bf8468f8dccafa3e5ba0a5a5c86bfff476034911
niravj <niravj@nirav-desktop.lan.coriolis.co.in>
2014-02-26 11:18:47 +0530

    Made some url changes.

--- mysite/upload/urls.py
+++ mysite/upload/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
+        url(r'^$', 'upload.views.login_page', name='login_page'),
         url(r'^login/$', 'upload.views.login_page', name='login_page'),
         url(r'^upload/$', 'upload.views.upload', name='upload'),
         url(r'logout/$', 'upload.views.logout_page', name='logout_page'),
