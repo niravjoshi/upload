1b14c45960dc0d1a6dbe3261eedf38049080e9e3
niravjoshi <vm-0.niravjoshi.koding.kd.io>
2014-03-06 14:35:51 +0000

    css fixed.

--- mysite/upload/constants.py
+++ mysite/upload/constants.py
@@ -5,4 +5,4 @@ FILEPATH = os.path.join(BASE_DIR,'..')
 SUBJECT = "UPLOAD: Please verify your email address"
 FROM = "Upload-Admin@gmail.com"
 
-URL = "http://nirav-desktop.lan.coriolis.co.in:8000/upload/"
+URL = "http://vm-0.niravjoshi.koding.kd.io:8888/upload/"
