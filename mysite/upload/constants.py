08f118934fb50d091cd694f64161999480881e15
niravjoshi <vm-0.niravjoshi.koding.kd.io>
2014-03-07 09:46:46 +0000

    removing extra code

--- mysite/upload/constants.py
+++ mysite/upload/constants.py
@@ -5,5 +5,5 @@ FILEPATH = os.path.join(BASE_DIR,'..')
 SUBJECT = "UPLOAD: Please verify your email address"
 FROM = "Upload-Admin@gmail.com"
 
-#URL = "http://vm-0.niravjoshi.koding.kd.io:8888/upload/"
-URL = "http://localhost:8000/upload/"
+URL = "http://vm-0.niravjoshi.koding.kd.io:8888/upload/"
+#URL = "http://localhost:8000/upload/"
